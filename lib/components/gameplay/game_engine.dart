7f69b33b5880fd00f130ff83b175bf740c6e6639
Leandro Schmidt <leandro.schmidt@ibrascan.com>
2023-08-15 08:50:08 -0300

    0.4.5.2

--- lib/components/gameplay/game_engine.dart
+++ lib/components/gameplay/game_engine.dart
@@ -21,6 +21,7 @@ import 'package:provider/provider.dart';
 class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, HasGameRef {
   //Variable Declarations
   late final BuildContext context;
+  late final Engine baseEngine;
   late final GameEngine engine;
   late final Gameplay gameplay;
   late final Options options;
@@ -38,9 +39,13 @@ class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, H
   Vector2 _position = Vector2(0.0, 0.0);
   get position => _position;
 
-  //Stop Ingame Connection
+  //Ingame Connection Variables
   bool _pauseConnection = false;
   get pauseConnection => _pauseConnection;
+  late dart.Timer _connection;
+  get connection => _connection;
+  int _msLag = 0;
+  get msLag => _msLag;
 
   //TO DO
   // Map _equipmentsLoaded = {};
@@ -67,6 +72,11 @@ class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, H
     _pauseConnection = value;
   }
 
+  //Close Connection
+  void closeConnection() {
+    _connection.cancel();
+  }
+
   //Change Player Position Variable
   void changePlayerPosition(Vector2 value) {
     _position = value;
@@ -89,11 +99,13 @@ class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, H
   @override
   void onMount() async {
     super.onMount();
+    baseEngine = Provider.of<Engine>(context, listen: false);
+    engine = Provider.of<GameEngine>(context, listen: false);
+    websocket = Provider.of<Websocket>(context, listen: false);
+    options = Provider.of<Options>(context, listen: false);
+    gameplay = Provider.of<Gameplay>(context, listen: false);
+    //Connection Signal
     Future<void> connectionSignal(context) async {
-      engine = Provider.of<GameEngine>(context, listen: false);
-      websocket = Provider.of<Websocket>(context, listen: false);
-      options = Provider.of<Options>(context, listen: false);
-      gameplay = Provider.of<Gameplay>(context, listen: false);
       await websocket.websocketSendIngame(
         {
           'message': 'login',
@@ -104,7 +116,173 @@ class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, H
           'token': options.token,
         },
         context,
-      );
+      ).then((value) {
+        //Connection Repeater
+        int timeoutHandle = 0;
+        _connection = dart.Timer.periodic(const Duration(milliseconds: 50), (timer) async {
+          if (!_pauseConnection) {
+            //Ping Delay Declaration
+            int connectionDelay = 0;
+            dart.Timer.periodic(const Duration(milliseconds: 1), (timer) {
+              connectionDelay++;
+            });
+
+            //Direction Translate
+            late final String direction;
+            if (engine.joystickPosition[0] < 0.0) {
+              direction = 'Direction.left';
+            }
+            if (engine.joystickPosition[0] > 0.0) {
+              direction = 'Direction.right';
+            }
+            if (engine.joystickPosition[0] == 0.0) {
+              direction = 'Direction.idle';
+            }
+
+            //Server Signal
+            final websocketMessage = await websocket.websocketSendIngame({
+              'message': 'playersPosition',
+              'id': options.id,
+              'positionX': position[0],
+              'positionY': position[1],
+              'direction': direction,
+              'location': gameplay.characters['character${gameplay.selectedCharacter}']['location'],
+              'class': gameplay.characters['character${gameplay.selectedCharacter}']['class'],
+            }, context);
+
+            //Loading Check
+            if (websocketMessage == "OK" || websocketMessage == "timeout") {
+              if (timeoutHandle > 100) {
+                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const MainMenu()), (route) => false);
+                websocket.disconnectWebsockets(context);
+                GlobalFunctions.errorDialog(
+                  errorMsgTitle: 'authentication_lost_connection',
+                  errorMsgContext: 'You have lost connection to the servers.',
+                  context: context,
+                );
+                return;
+              }
+              return;
+            }
+
+            //Result
+            final players = jsonDecode(websocketMessage);
+            final enemy = jsonDecode(websocketMessage)['enemy'];
+
+            //Update Users
+            if (players != {}) {
+              players.remove("enemy");
+              //Store old Values
+              List oldUsers = [];
+              gameplay.usersInWorld.forEach((key, value) => oldUsers.add(value));
+
+              //Clean
+              gameplay.usersHandle('replace', players);
+
+              //Load New Values
+              List users = [];
+              gameplay.usersInWorld.forEach((key, value) => users.add(value));
+              List idRemove = [];
+              List idAdd = [];
+              //Add Function
+              if (oldUsers.length != users.length) {
+                //Sweep Users
+                for (int i = 0; i < users.length; i++) {
+                  bool add = true;
+                  //Add Sweep
+                  if (oldUsers.length < users.length) {
+                    for (int j = 0; j < oldUsers.length; j++) {
+                      //If already exist
+                      if (users[i]['id'] == oldUsers[j]['id']) {
+                        add = false;
+                        break;
+                      }
+                    }
+                  } else {
+                    add = false;
+                  }
+                  //Add if not exist
+                  if (add && users[i]['positionX'] != null && users[i]['positionY'] != null) {
+                    idAdd.add(users[i]['id']);
+                    final positionX = double.parse(users[i]['positionX'].toString());
+                    final positionY = double.parse(users[i]['positionY'].toString());
+                    if (users[i]['id'] != options.id) {
+                      gameRef.add(PlayerClient(users[i]['id'].toString(), Vector2(positionX, positionY), context));
+                    }
+                  }
+                }
+                //Sweep Old Users
+                for (int i = 0; i < oldUsers.length; i++) {
+                  //Find if no longer online
+                  bool remove = true;
+                  for (int j = 0; j < users.length; j++) {
+                    if (oldUsers[i]['id'] == users[j]['id']) {
+                      remove = false;
+                      break;
+                    }
+                  }
+                  //Remove if no longer online
+                  if (remove) {
+                    idRemove.add(oldUsers[i]['id']);
+                  }
+                }
+              }
+            }
+            //Update Enemies
+            if (enemy != {} && enemy != null) {
+              //Store old Values
+              List oldEnemies = [];
+              gameplay.enemiesInWorld.forEach((key, value) => oldEnemies.add(value));
+
+              //Clean
+              gameplay.enemyHandle('replace', enemy);
+
+              //Load New Values
+              List enemies = [];
+              gameplay.enemiesInWorld.forEach((key, value) => enemies.add(value));
+
+              //Add Function
+              if (oldEnemies.length != enemies.length) {
+                try {
+                  //Sweep enemies
+                  for (int i = 0; i < enemies.length; i++) {
+                    bool add = true;
+                    //Add Sweep
+                    if (oldEnemies.length < enemies.length) {
+                      for (int j = 0; j < oldEnemies.length; j++) {
+                        //If already exist
+                        if (enemies[i]['id'] == oldEnemies[j]['id']) {
+                          add = false;
+                          break;
+                        }
+                      }
+                    } else {
+                      add = false;
+                    }
+                    //Add if not exist
+                    if (add && enemies[i]['positionX'] != null && enemies[i]['positionY'] != null) {
+                      //Add
+                      gameRef.add(
+                        EnemyWithVision(
+                          context,
+                          int.parse(enemies[i]['id'].toString()),
+                          enemies[i]['name'],
+                          80.0,
+                          20.0,
+                          size: Vector2.all(32.0),
+                        ),
+                      );
+                    }
+                  }
+                } catch (_) {}
+              }
+            }
+
+            //Ping Delay Update
+            _msLag = connectionDelay;
+          }
+        });
+      });
     }
 
     await connectionSignal(context);
@@ -112,23 +290,18 @@ class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, H
 
   @override
   Future<void> onLoad() async {
-    final engine = Provider.of<Engine>(context, listen: false);
-    //Joystick and Player Creation
-    //Load Sprite
-    images.load('interface/joystick.png').then((value) {
-      //Player Creation
-      final player = Player(context, Vector2(48.0, 180.0));
+    //Player Creation
+    final player = Player(context, Vector2(48.0, 180.0));
 
-      //Add Components
-      add(player);
-      //Camera
-      // ignore: deprecated_member_use
-      camera.followVector2(player.cameraPosition);
-    });
+    //Add Components
+    add(player);
+    //Camera
+    // ignore: deprecated_member_use
+    camera.followVector2(player.cameraPosition);
     //World Loading
     MySQLGameplay.returnLevel(context: context, level: MySQL.returnInfo(context, returned: 'location')).then((value) {
       final worldGeneration = WorldGeneration();
-      worldGeneration.generateWorld(value, engine.gameController);
+      worldGeneration.generateWorld(value, baseEngine.gameController);
     });
     //Test
     // final worldGeneration = WorldGeneration();
@@ -144,164 +317,5 @@ class GameEngine extends FlameGame with HasCollisionDetection, ChangeNotifier, H
     //     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     //   ]
     // ], engine.gameController);
-  }
-
-  @override
-  void update(double dt) async {
-    super.update(dt);
-    //Connection Signal
-    int timeoutHandle = 0;
-    dart.Timer.periodic(const Duration(milliseconds: 50), (timer) async {
-      if (!_pauseConnection) {
-        //Direction Translate
-        late final String direction;
-        if (engine.joystickPosition[0] < 0.0) {
-          direction = 'Direction.left';
-        }
-        if (engine.joystickPosition[0] > 0.0) {
-          direction = 'Direction.right';
-        }
-        if (engine.joystickPosition[0] == 0.0) {
-          direction = 'Direction.idle';
-        }
-
-        final websocketMessage = await websocket.websocketSendIngame({
-          'message': 'playersPosition',
-          'id': options.id,
-          'positionX': position[0],
-          'positionY': position[1],
-          'direction': direction,
-          'location': gameplay.characters['character${gameplay.selectedCharacter}']['location'],
-          'class': gameplay.characters['character${gameplay.selectedCharacter}']['class'],
-        }, context);
-        //Loading Check
-        if (websocketMessage == "OK" || websocketMessage == "timeout") {
-          if (timeoutHandle > 100) {
-            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const MainMenu()), (route) => false);
-            websocket.disconnectWebsockets(context);
-            GlobalFunctions.errorDialog(
-              errorMsgTitle: 'authentication_lost_connection',
-              errorMsgContext: 'You have lost connection to the servers.',
-              context: context,
-            );
-            return;
-          }
-          return;
-        }
-
-        //Result
-        final players = jsonDecode(websocketMessage);
-        final enemy = jsonDecode(websocketMessage)['enemy'];
-
-        //Update Users
-        if (players != {}) {
-          players.remove("enemy");
-          //Store old Values
-          List oldUsers = [];
-          gameplay.usersInWorld.forEach((key, value) => oldUsers.add(value));
-
-          //Clean
-          gameplay.usersHandle('replace', players);
-
-          //Load New Values
-          List users = [];
-          gameplay.usersInWorld.forEach((key, value) => users.add(value));
-          List idRemove = [];
-          List idAdd = [];
-          //Add Function
-          if (oldUsers.length != users.length) {
-            //Sweep Users
-            for (int i = 0; i < users.length; i++) {
-              bool add = true;
-              //Add Sweep
-              if (oldUsers.length < users.length) {
-                for (int j = 0; j < oldUsers.length; j++) {
-                  //If already exist
-                  if (users[i]['id'] == oldUsers[j]['id']) {
-                    add = false;
-                    break;
-                  }
-                }
-              } else {
-                add = false;
-              }
-              //Add if not exist
-              if (add && users[i]['positionX'] != null && users[i]['positionY'] != null) {
-                idAdd.add(users[i]['id']);
-                final positionX = double.parse(users[i]['positionX'].toString());
-                final positionY = double.parse(users[i]['positionY'].toString());
-                if (users[i]['id'] != options.id) {
-                  gameRef.add(PlayerClient(users[i]['id'].toString(), Vector2(positionX, positionY), context));
-                }
-              }
-            }
-            //Sweep Old Users
-            for (int i = 0; i < oldUsers.length; i++) {
-              //Find if no longer online
-              bool remove = true;
-              for (int j = 0; j < users.length; j++) {
-                if (oldUsers[i]['id'] == users[j]['id']) {
-                  remove = false;
-                  break;
-                }
-              }
-              //Remove if no longer online
-              if (remove) {
-                idRemove.add(oldUsers[i]['id']);
-              }
-            }
-          }
-        }
-        //Update Enemies
-        if (enemy != {} && enemy != null) {
-          //Store old Values
-          List oldEnemies = [];
-          gameplay.enemiesInWorld.forEach((key, value) => oldEnemies.add(value));
-
-          //Clean
-          gameplay.enemyHandle('replace', enemy);
-
-          //Load New Values
-          List enemies = [];
-          gameplay.enemiesInWorld.forEach((key, value) => enemies.add(value));
-
-          //Add Function
-          if (oldEnemies.length != enemies.length) {
-            try {
-              //Sweep enemies
-              for (int i = 0; i < enemies.length; i++) {
-                bool add = true;
-                //Add Sweep
-                if (oldEnemies.length < enemies.length) {
-                  for (int j = 0; j < oldEnemies.length; j++) {
-                    //If already exist
-                    if (enemies[i]['id'] == oldEnemies[j]['id']) {
-                      add = false;
-                      break;
-                    }
-                  }
-                } else {
-                  add = false;
-                }
-                //Add if not exist
-                if (add && enemies[i]['positionX'] != null && enemies[i]['positionY'] != null) {
-                  //Add
-                  gameRef.add(
-                    EnemyWithVision(
-                      context,
-                      int.parse(enemies[i]['id'].toString()),
-                      enemies[i]['name'],
-                      80.0,
-                      20.0,
-                      size: Vector2.all(32.0),
-                    ),
-                  );
-                }
-              }
-            } catch (_) {}
-          }
-        }
-      }
-    });
   }
 }
