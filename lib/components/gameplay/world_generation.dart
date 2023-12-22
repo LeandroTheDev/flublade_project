a3d341770e568ca3194582b7583cb718fe30779a
Leandro Schmidt <leandro.schmidt@ibrascan.com>
2023-12-22 08:19:31 -0300

    0.6.0

--- lib/components/gameplay/world_generation.dart
+++ lib/components/gameplay/world_generation.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
 //
 //  DOCS
@@ -56,23 +57,80 @@ class WorldGeneration extends SpriteComponent {
     },
   ];
 
+  ///Return number to multiply the start position of chunk render
+  ///need the double of square root to me stringify
+  int calculateChunkNegative(String chunkSquareRoot) {
+    //Will be better if i make a calculation to plus 2 add +1 but i will make this in future
+    switch (chunkSquareRoot) {
+      case "3.0":
+        return 1;
+      case "5.0":
+        return 2;
+      case "7.0":
+        return 3;
+      case "9.0":
+        return 4;
+      case "11.0":
+        return 5;
+      case "13.0":
+        return 6;
+      case "15.0":
+        return 7;
+      case "17.0":
+        return 8;
+      case "19.0":
+        return 9;
+      case "21.0":
+        return 10;
+    }
+    return 0;
+  }
+
   ///Receives the world data and the gameController to add the tiles in the world
   List generateWorld(List<dynamic> worldData, FlameGame gameController, Vector2 playerPosition) {
     List tilesRendered = [];
     final chunkSquareRoot = sqrt(worldData.length);
+    // print("Chunk Square Root: $chunkSquareRoot, chunk quantity: ${worldData.length}"); //DEBUG
+    int chunkPosition = 1;
+    //Chunk Render Calculation Variables // Less 2 to exclude internal radius
+    final chunkNegativeCalculation = (480 * calculateChunkNegative(chunkSquareRoot.toString()));
+    double startX = playerPosition[0] - chunkNegativeCalculation;
+    double startY = playerPosition[1] - chunkNegativeCalculation;
+    double actualStartX = startX;
+    double actualStartY = startY;
+    // print("Starting Render Position: $startX, $startY"); //DEBUG
     //Swipe all chunks
-    double startX = 0;
-    double startY = 0;
     for (int chunkIndex = 0; chunkIndex < chunkSquareRoot * chunkSquareRoot; chunkIndex++) {
+      // print("Rendering the chunk: $chunkIndex"); //DEBUG
+      //Checking the actual positiion of chunk to see if we need to down the position
+      if (chunkPosition > chunkSquareRoot) {
+        //Reseting the position
+        chunkPosition = 1;
+        //Reseting the base X because we finished the X chunks now we are going lower
+        startX = playerPosition[0] - chunkNegativeCalculation;
+        actualStartX = startX;
+        //Incresing the base Y because the X is finished and we need to go lower in the coordinates
+        actualStartY += 480;
+        startY = actualStartY;
+      }
+      // print("Chunk render startX: $startX, startY: $startY"); //DEBUG
+      //Increase the chunk position
+      chunkPosition++;
       tilesRendered.add([]);
       //Pickup the actual chunk by index
       final actualChunk = worldData[chunkIndex];
       //Check if chunk is not null, null when chunk doesnt exist
-      if (actualChunk == null) continue;
+      if (actualChunk == null) {
+        //Change chunk X position to the next chunk
+        startX += 480;
+        actualStartX = startX;
+        continue;
+      }
       //Actual tiles for the specific chunk
       final List actualTiles = jsonDecode(actualChunk["tiles"]);
       //Swiping the Y from the chunk
       for (int y = 0; y < actualTiles.length; y++) {
+        // print("X: ${startX}, Y: ${startY}");
         //Swiping the X from the chunk
         for (int x = 0; x < actualTiles[y].length; x++) {
           //Create the component
@@ -89,11 +147,15 @@ class WorldGeneration extends SpriteComponent {
           tilesRendered[chunkIndex].add(component);
           startX += 32;
         }
+        //Reseting the X to render the lower tiles
+        startX = actualStartX;
+        //Incresing the Y to render lower tiles
         startY += 32;
-        startX = 0;
       }
+      actualStartX += 480;
+      startY = actualStartY;
     }
-    return worldTiles;
+    return tilesRendered;
   }
 
   ///Removes all Components in the list from the render
